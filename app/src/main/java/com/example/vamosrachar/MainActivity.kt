db045a296dda1aa3553b9032be17bb4a2a433f46
Paulo Nemer <93223341+paulonemer@users.noreply.github.com>
2023-05-15 21:02:44 -0300

    está funcional

--- app/src/main/java/com/example/vamosrachar/MainActivity.kt
+++ app/src/main/java/com/example/vamosrachar/MainActivity.kt
@@ -3,17 +3,24 @@ package com.example.vamosrachar
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.speech.tts.TextToSpeech
 import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import java.util.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
+    private lateinit var tts: TextToSpeech
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
         var valor = findViewById<TextView>(R.id.valor)
+        tts = TextToSpeech(this, this)
+
         fun compartilharTexto(texto: String) {
             val intent = Intent(Intent.ACTION_SEND)
             intent.type = "text/plain"
@@ -45,9 +52,24 @@ class MainActivity : AppCompatActivity() {
             share.setOnClickListener(View.OnClickListener {
                 compartilharTexto("${valor.text.toString()}")
             })
+        val falar: Button = findViewById(R.id.falar)
+            falar.setOnClickListener(View.OnClickListener {
+            tts?.speak(valor.text.toString(), TextToSpeech.QUEUE_FLUSH, null, "")
+        })
 
 
 
+    }
+
+    override fun onInit(status: Int) {
+        if (status == TextToSpeech.SUCCESS) {
+            val result = tts.setLanguage(Locale("pt", "BR"))
+            if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Log.e("TextToSpeech", "Idioma não suportado")
+            }
+        } else {
+            Log.e("TextToSpeech", "Falha na inicialização")
+        }
     }
 
 
